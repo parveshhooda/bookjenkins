3e89eed57ddb0faa39ab85138b163ff2c08e9a9c
Parvesh Hooda <parveshhooda81>
2021-07-24 01:30:49 +0530

    first commit

---
+++ src/main/java/com/axis/Bookcontroller.java
@@ -0,0 +1,16 @@
+package com.axis;
+
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RestController;
+
+	@RestController
+	@RequestMapping("api/v1/welcome")
+	public class Bookcontroller {
+		
+		@GetMapping
+		public String welcome() {
+			return "Welcome to Axis bank";
+		}
+	}
+
